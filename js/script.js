a40cd666afef0a6f0311d7573c9b79485f4cf430
ch-iqra-ashraf <iqraaashraf97@gmail.com>
2026-06-25 15:39:36 +0500

    added js logic

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const displayResult = document.querySelector("#display");
+const clearBtn = document.querySelector("#clear");
+const operatorBtn = document.querySelectorAll(".operator");
+const digitBtnList = document.querySelectorAll(".digit");
+const decimalBtn = document.querySelector("#decimal");
+const equalBtn = document.querySelector("#equal");
+const delBtn = document.querySelector("#delete");
+
+digitBtnList.forEach((digit) => {
+    digit.addEventListener("click", () => {
+        let current = displayResult.value;
+        displayResult.value = `${current}${digit.textContent}`;
+    });
+});
+
+operatorBtn.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        let current = displayResult.value;
+        displayResult.value = `${current}${operator.textContent}`;
+    });
+});
+
+delBtn.addEventListener("click", () => {
+    displayResult.value = displayResult.value.slice(0, -1);
+});
+
+
+equalBtn.addEventListener("click", () => {
+    let result = eval(displayResult.value);
+    displayResult.value = result;
+});
+
+clearBtn.addEventListener("click", () => {
+    displayResult.value = "";
+});
+
+
+
